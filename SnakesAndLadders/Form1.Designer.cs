f037d358caaa6088d456e064d942951c8ef98bdb
jessicapeck <37042385+jessicapeck@users.noreply.github.com>
2022-02-25 10:50:35 +0000

    created 'snakes and ladders' board with alternate black and white squares with appropriate number

--- SnakesAndLadders/Form1.Designer.cs
+++ SnakesAndLadders/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace SnakesAndLadders
 {
-    partial class Form1
+    partial class Form
     {
         /// <summary>
         /// Required designer variable.
@@ -29,13 +29,38 @@ namespace SnakesAndLadders
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.Color.Red;
+            this.panel.Location = new System.Drawing.Point(0, 0);
+            this.panel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(400, 400);
+            this.panel.TabIndex = 0;
+            this.panel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Paint);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.panel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form";
+            this.Text = "Snakes and Ladders";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel;
     }
 }
 
--- SnakesAndLadders/Form1.cs
+++ SnakesAndLadders/Form1.cs
@@ -10,11 +10,69 @@ using System.Windows.Forms;
 
 namespace SnakesAndLadders
 {
-    public partial class Form1 : Form
+    public partial class Form : System.Windows.Forms.Form
     {
-        public Form1()
+
+        int num_rows = 10;
+        int num_cols = 10;
+
+        private SolidBrush sb_black = new SolidBrush(Color.Black);
+        private SolidBrush sb_white = new SolidBrush(Color.White);
+
+        public Form()
         {
             InitializeComponent();
+        }
+
+        private void panel_Paint(object sender, PaintEventArgs e)
+        {
+            int pixel_width = panel.ClientSize.Width / num_cols;
+            int pixel_height = panel.ClientSize.Height / num_rows;
+
+            Graphics targetGraphics = e.Graphics;
+
+            int square_num = 101;
+
+            for (int row_counter = 0; row_counter < num_cols; row_counter++)
+            {
+                square_num -= 10;
+
+                for (int col_counter = 0; col_counter < num_rows; col_counter++)
+                {
+                    Color num_color = new Color();
+
+                    if (((row_counter % 2 == 0) && (col_counter % 2 == 1)) || ((row_counter % 2 == 1) && (col_counter % 2 == 0)))
+                    {
+                        targetGraphics.FillRectangle(sb_black,col_counter * pixel_width, row_counter * pixel_height, pixel_width, pixel_height);
+
+                        num_color = Color.White;                        
+                    }
+                    else
+                    {
+                        targetGraphics.FillRectangle(sb_white, col_counter * pixel_width, row_counter * pixel_height, pixel_width, pixel_height);
+
+                        num_color = Color.Black;
+                    }
+
+                    using (Font font = new Font("Microsoft Sans Serif", 10, FontStyle.Bold, GraphicsUnit.Pixel))
+                    {
+                        Point point = new Point(col_counter * pixel_width, row_counter * pixel_height);
+                        TextRenderer.DrawText(e.Graphics, square_num.ToString(), font, point, num_color);
+                    }
+
+                    if (col_counter != num_cols - 1)
+                    {
+                        if (row_counter % 2 == 0)
+                        {
+                            square_num += 1;
+                        }
+                        else if (row_counter % 2 == 1)
+                        {
+                            square_num -= 1;
+                        }
+                    }
+                }
+            }
         }
     }
 }
